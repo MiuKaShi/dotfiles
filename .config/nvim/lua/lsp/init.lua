4f2b126a58dd743781794bb5fab674abc9c020fa
MiuKaShi <d.lolimiuku@gmail.com>
2022-03-17 03:43:05 +0900

    update lsp setting

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -45,8 +45,110 @@ lspconfig.gopls.setup{
 lspconfig.clangd.setup{
     capabilities = capabilities,
     on_attach = on_attach,
-    filetypes = { "c", "cpp", "cc" },
+    filetypes = {'c', 'cpp', 'cc'},
     flags = {debounce_text_changes = 150}
+}
+
+-- cssls
+lspconfig.cssls.setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = {'vscode-css-languageserver', '--stdio'},
+    flags = {debounce_text_changes = 150}
+}
+
+-- yamlls
+lspconfig.yamlls.setup{
+    on_attach = on_attach,
+    settings = {
+        yaml = {
+            schemas = {
+                ['https://cfn-schema.y13i.com/schema?region=eu-west-2&version=20.0.0'] = 'cloudformation/*',
+                ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*'
+            },
+            format = {enable = false},
+            completion = true,
+            hover = true,
+            customTags = {
+                '!And scalar',
+                '!And mapping',
+                '!And sequence',
+                '!Condition scalar',
+                '!Condition mapping',
+                '!Condition sequence',
+                '!Base64 scalar',
+                '!Base64 mapping',
+                '!Base64 sequence',
+                '!If scalar',
+                '!If mapping',
+                '!If sequence',
+                '!Not scalar',
+                '!Not mapping',
+                '!Not sequence',
+                '!Equals scalar',
+                '!Equals mapping',
+                '!Equals sequence',
+                '!Or scalar',
+                '!Or mapping',
+                '!Or sequence',
+                '!FindInMap scalar',
+                '!FindInMap mappping',
+                '!FindInMap sequence',
+                '!Base64 scalar',
+                '!Base64 mapping',
+                '!Base64 sequence',
+                '!Cidr scalar',
+                '!Cidr mapping',
+                '!Cidr sequence',
+                '!Ref scalar',
+                '!Ref mapping',
+                '!Ref sequence',
+                '!Sub scalar',
+                '!Sub mapping',
+                '!Sub sequence',
+                '!GetAtt scalar',
+                '!GetAtt mapping',
+                '!GetAtt sequence',
+                '!GetAZs scalar',
+                '!GetAZs mapping',
+                '!GetAZs sequence',
+                '!ImportValue scalar',
+                '!ImportValue mapping',
+                '!ImportValue sequence',
+                '!Select scalar',
+                '!Select mapping',
+                '!Select sequence',
+                '!Split scalar',
+                '!Split mapping',
+                '!Split sequence',
+                '!Join scalar',
+                '!Join mapping',
+                '!Join sequence'
+            }
+        }
+    }
+}
+
+-- jsonls
+lspconfig.jsonls.setup{
+    on_attach = function(client)
+        client.resolved_capabilities.document_formatting = false
+        on_attach(client)
+    end,
+    cmd = {'vscode-json-languageserver', '--stdio'},
+    settings = {
+        json = {
+            format = {enable = false},
+            schemaDownload = {enable = true},
+            schemas = {
+                {
+                    description = 'AWS IAM configuration',
+                    fileMatch = {'iam/*.json'},
+                    url = 'https://gist.githubusercontent.com/jstewmon/ee5d4b7ec0d8d60cbc303cb515272f8a/raw/fc6977788b85ea52e9acad0347287516157b5865/aws-iam-poilcy-schema.json'
+                }
+            }
+        }
+    }
 }
 
 -- bash
@@ -58,15 +160,67 @@ lspconfig.pyright.setup{on_attach = on_attach, capabilities = capabilities}
 -- vimls
 lspconfig.vimls.setup{on_attach = on_attach, capabilities = capabilities}
 
+-- lua
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+lspconfig.sumneko_lua.setup({
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {version = 'LuaJIT', path = runtime_path},
+            diagnostics = {globals = {'vim'}},
+            workspace = {library = vim.api.nvim_get_runtime_file('', true)},
+            telemetry = {enable = false}
+        }
+    },
+    capabilities = capabilities
+})
+
 -- Linters and formatters (efm-lang-server)
+local prettier = function(parameters)
+    parameters = parameters or ''
+    return {
+        formatCommand = 'prettier --stdin-filepath ${INPUT} ' .. parameters,
+        formatStdin = true
+    }
+end
 lspconfig.efm.setup{
     on_attach = on_attach,
     capabilities = capabilities,
     init_options = {documentFormatting = true},
-    filetypes = {'matlab', 'fish', 'lua', 'python', 'pandoc'},
+    filetypes = {
+        'json',
+        'java',
+        'yaml',
+        'sh',
+        'matlab',
+        'markdown',
+        'lua',
+        'python'
+    },
     settings = {
         rootMarkers = {'.git/'},
         languages = {
+            java = {prettier()},
+            json = {prettier()},
+            yaml = {
+                prettier('--single-quote'),
+                {
+                    lintCommand = 'yamllint -f parsable -',
+                    lintStdin = true,
+                    lintFormats = {'%f:%l %m'}
+                }
+            },
+            sh = {
+                {
+                    formatCommand = 'shfmt -filename ${INPUT}',
+                    formatStdin = true,
+                    lintCommand = 'shellcheck -f gcc -x',
+                    lintSource = 'shellcheck'
+                }
+            },
             matlab = {
                 {
                     lintCommand = '/usr/bin/mlint -severity',
@@ -85,20 +239,11 @@ lspconfig.efm.setup{
                     }
                 }
             },
-            fish = {
+            markdown = {
                 {
-                    lintCommand = '/usr/bin/fish -n',
-                    lintStdin = false,
-                    lintStderr = true,
-                    lintFormats = {'%f (line %l): %m'}
-                }
-            },
-            pandoc = {
-                {
-                    lintCommand = 'markdownlint -s',
+                    lintCommand = 'markdownlint -s -c ${HOME}/.markdownlintrc',
                     lintStdin = true,
-                    lintFormats = {'%f:%l %m', '%f:%l:%c %m', '%f: %l: %m'},
-                    formatCommand = 'pandoc -f markdown -t markdown -sp --tab-stop=2'
+                    lintFormats = {'%f:%l %m', '%f:%l:%c %m', '%f: %l: %m'}
                 }
             },
             lua = {{formatCommand = 'lua-format -i', formatStdin = true}},
