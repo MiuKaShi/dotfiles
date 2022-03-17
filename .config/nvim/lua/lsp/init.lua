d747faa2ed50daa5c044b25c301515d3826a985c
MiuKaShi <d.lolimiuku@gmail.com>
2022-03-17 12:25:05 +0900

    add markdownlintrc

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -241,7 +241,7 @@ lspconfig.efm.setup{
             },
             markdown = {
                 {
-                    lintCommand = 'markdownlint -s -c ${HOME}/.markdownlintrc',
+                    lintCommand = 'markdownlint -s -c ${HOME}/.config/.markdownlintrc',
                     lintStdin = true,
                     lintFormats = {'%f:%l %m', '%f:%l:%c %m', '%f: %l: %m'}
                 }
