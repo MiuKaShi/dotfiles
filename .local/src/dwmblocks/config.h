32140f47205236efce1a2ff223bf88e382378406
miuka <d.lolimiuku@gmail.com>
2022-04-22 20:52:12 +0900

    small changes

--- .local/src/dwmblocks/config.h
+++ .local/src/dwmblocks/config.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"⌨", "sb-kbselect", 0, 30}, */
+	// {"⌨", "sb-kbselect", 0, 30}, 
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"sb-tasks",	10,	26},
 	{"",	"sb-music",	0,	11},
@@ -24,7 +24,7 @@ static const Block blocks[] = {
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
-	{"",	"sb-help-icon",	0,	15},
+	// {"",	"sb-help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
