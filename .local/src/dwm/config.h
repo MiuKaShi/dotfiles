ceb7a0028311dadf2e245c5650edebf074818d9d
miuka <d.lolimiuku@gmail.com>
2022-05-16 19:38:44 +0900

    a little change

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -5,7 +5,7 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static unsigned int borderpx = 3;               /* border pixel of windows */
+static unsigned int borderpx = 4;               /* border pixel of windows */
 static unsigned int snap = 32;                  /* snap pixel */
 static const unsigned int systraypinning =
     0;                                      /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -15,8 +15,8 @@ static const int systraypinningfailfirst =
     1;                                     /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray = 1;               /* 0 means no systray */
 static unsigned int gappih = 20;                /* horiz inner gap between windows */
-static unsigned int gappiv = 10;                /* vert inner gap between windows */
-static unsigned int gappoh = 10;                /* horiz outer gap between windows and screen edge */
+static unsigned int gappiv = 20;                /* vert inner gap between windows */
+static unsigned int gappoh = 20;                /* horiz outer gap between windows and screen edge */
 static unsigned int gappov = 30;                /* vert outer gap between windows and screen edge */
 static int swallowfloating = 0;                 /* 1 means swallow floating windows by default */
 static int smartgaps = 0;                       /* 1 means no outer gap when there is only one window */
