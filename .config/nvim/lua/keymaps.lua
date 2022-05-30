1e1369750e08e35793e469f250f9b87c28c803be
miuka <d.lolimiuku@gmail.com>
2022-05-30 13:46:12 +0900

    keymaps change

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -155,7 +155,7 @@ wk.register({
         f = { '<cmd>lua vim.lsp.buf.range_formatting()<cr>', "format", noremap = false, mode = "v" },
     },
     t           = { ':Lspsaga open_floaterm<CR>', 'Open Terminal(exit with <ESC>)' }, -- 打开终端
-    h           = { ':call ToggleHiddenAll()<CR>]', 'Toggle Statusbar/LSP' }, -- 项目内查找
+    l           = { ':call ToggleHiddenAll()<CR>]', 'LSP Toggle' }, -- LSP 开关
     ['*']       = { "<cmd>lua require('telescope.builtin').lsp_references()<cr>", 'Search reference in current project' }, -- lsp 查找引用
     ['/']       = { ':Telescope live_grep<CR>', 'Fuzzy search in project' }, -- 项目内查找
     ['!']       = { ':Telescope help_tags theme=ivy<CR>', 'Help commands by fuzzy search' }, -- vim帮助查找
