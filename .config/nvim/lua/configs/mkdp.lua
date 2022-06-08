dd4abd11d0ff12065c375804e39570589e94755d
miuka <d.lolimiuku@gmail.com>
2022-06-08 14:29:31 +0900

    markdown preview open new window

--- .config/nvim/lua/configs/mkdp.lua
+++ .config/nvim/lua/configs/mkdp.lua
@@ -1,7 +1,13 @@
-vim.g.mkdp_browser = 'firefox'
+vim.cmd [[
+function! g:Open_browser(url)
+    silent exec "!st -e surf " . a:url . " &"
+endfunction
+]]
+
+vim.g.mkdp_browserfunc = 'g:Open_browser'
 vim.g.mkdp_page_title = '${name}.md'
 vim.g.mkdp_filetypes = {'markdown'}
 vim.g.mkdp_preview_options = {
-  ['sync_scroll_type'] = 'relative',
+  -- ['sync_scroll_type'] = 'relative',
   ['hide_yaml_meta'] = 1,
 }
