ee8b3503ac70446403ad6fee34bfed7dcbb7b7d6
miuka <d.lolimiuku@gmail.com>
2022-06-14 22:44:52 +0900

    fix warning multiple offset_decoding

--- .config/nvim/lua/lsp/languages/clangd.lua
+++ .config/nvim/lua/lsp/languages/clangd.lua
@@ -4,7 +4,12 @@ M.setup = function(on_attach, capabilities)
     require('lspconfig').clangd.setup {
         on_attach = on_attach,
         capabilities = capabilities,
-        filetypes = { 'c', 'cpp', 'cc' },
+        filetypes = {
+            "c",
+            "cpp",
+            "objc",
+            "objcpp",
+        };
         flags = { debounce_text_changes = 150 },
     }
 end
