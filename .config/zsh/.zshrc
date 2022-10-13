3efd0c8ae8d29c56fe7050685c2a298d093240e6
miuka <d.lolimiuku@gmail.com>
2022-10-13 20:46:20 +0900

    update some configs

--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -8,6 +8,7 @@ autoload -U colors && colors	# Load colors
 PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
 setopt autocd		# Automatically cd into typed directory.
 stty stop undef		# Disable ctrl-s to freeze terminal.
+unsetopt nomatch    # Fix url quote
 setopt interactive_comments
 
 # ZSH History:
--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -7,12 +7,10 @@ Base.atreplinit() do repl
         @async @eval using Revise
         @async @eval using BenchmarkTools
         import OhMyREPL as OMR
-        import Crayons as C
         promptfn() = "(" * splitpath(Base.active_project())[end-1] * ") julia> "
         OMR.input_prompt!(promptfn)
         OMR.colorscheme!("GruvboxDark")
         OMR.enable_pass!("RainbowBrackets", true)
-        OMR.Passes.BracketHighlighter.setcrayon!(C.Crayon(foreground=:blue))
         OMR.Passes.RainbowBrackets.activate_256colors()
     end
 end
