71ab445da5b1b17c957fb1f7765230d4da2ec40e
miuka <d.lolimiuku@gmail.com>
2023-02-08 02:07:20 +0900

    updatebib: fix BIB path

--- .config/script/updatebib.sh
+++ .config/script/updatebib.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
+BIB="/home/miuka/papers/bib/mybib.bib"
+
 cd "${BIB%/*}" || exit 1
-grep -wv @preamble "$BIB" | python journal_abbrev.py > mybib_abb.bib
+grep -wv @preamble "$BIB" | python journal_abbrev.py >mybib_abb.bib
 notify-send "Bib_abbrev Update"
-
-
-
