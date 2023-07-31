ee8db4682810ac947792ba5117fabd1ffc1930d6
miuka <d.lolimiuku@gmail.com>
2023-07-31 15:00:09 +0900

    add new gitignore

--- .local/.local/src/xmenu/xmenu.sh
+++ .local/.local/src/xmenu/xmenu.sh
@@ -4,6 +4,8 @@ xmenu <<EOF | sh &
 Applications
 	IMG:./icons/web.png	Web Browser	firefox
 	IMG:./icons/gimp.png	Image editor	gimp
+Terminal (xterm)	xterm
+Terminal (urxvt)	urxvt
 Terminal (st)		st
 
 Shutdown		poweroff
