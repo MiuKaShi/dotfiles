6f856c7fa9ca7e6e808f65999883c57848259525
miuka <d.lolimiuku@gmail.com>
2023-09-19 21:41:02 +0900

    update ipe setting

--- .config/.config/ipe/ipelets/customze.lua
+++ .config/.config/ipe/ipelets/customze.lua
@@ -1,32 +1,48 @@
-prefs.external_editor = 'code --wait %s'
+prefs.external_editor = "code --wait %s"
 prefs.max_zoom = 200
-prefs.tex_engine = 'xetex'
+prefs.tex_engine = "xetex"
 prefs.spell_check = false
 prefs.auto_run_latex = true
 prefs.autosave_interval = nil
-prefs.start_screen = 'maximized'
+prefs.start_screen = "maximized"
 prefs.normal_resolution = 92
-prefs.scroll['pan'] = false
-
--- shortcuts
-shortcuts.zoom_in = 'Ctrl+='
-shortcuts.zoom_out = 'Ctrl+-'
-shortcuts.fit_page = ','
-shortcuts.mode_pan = 'C'
-shortcuts.mode_shredder = 'W'
-shortcuts.mode_math = 'Shift+M'
-shortcuts.ipelet_1_symbols = 'Shift+S'
-shortcuts.mode_splines = 'Alt+Ctrl+I'
+prefs.scroll["pan"] = false
 prefs.scroll.vertical_sign = -1
-
-shortcuts['horizontalalignment|left'] = 'Alt+Shift+L'
-shortcuts['horizontalalignment|hcenter'] = 'Alt+Shift+H'
-shortcuts['horizontalalignment|right'] = 'Alt+Shift+R'
-
-shortcuts['verticalalignment|bottom'] = 'Alt+Shift+B'
-shortcuts['verticalalignment|baseline'] = 'Alt+B'
-shortcuts['verticalalignment|vcenter'] = 'Alt+Shift+C'
-shortcuts['verticalalignment|top'] = 'Alt+Shift+T'
+prefs.ui_scale = 200
+prefs.toolbar_scale = 200
 
 -- Substitute with absolute /path/to/ElectricalEngineering.isy
-prefs.styles = { 'doctor_xe_en' }
+prefs.styles = { "basic", "color", "symbol", "latex" }
+
+-- shortcuts
+shortcuts.mode_select = "1"
+shortcuts.mode_translate = "2"
+shortcuts.mode_rotate = "3"
+shortcuts.mode_stretch = "4"
+
+shortcuts.mode_pan = "`"
+shortcuts.mode_rectangles1 = "R"
+shortcuts.mode_label = "T"
+shortcuts.mode_lines = "B"
+shortcuts.mode_circle1 = "E"
+shortcuts.mode_shredder = "X"
+shortcuts.pan_here = "Z"
+shortcuts.mode_math = "Shift+M"
+shortcuts.mode_splines = "S"
+shortcuts.pick_properties = "Q"
+shortcuts.apply_properties = "Ctrl+Q"
+shortcuts.zoom_in = "Ctrl+="
+shortcuts.zoom_out = "Ctrl+-"
+shortcuts.delete = "backspace"
+
+shortcuts.fit_page = ","
+shortcuts.ipelet_1_symbols = "Shift+S"
+
+shortcuts["horizontalalignment|left"] = "Alt+Shift+L"
+shortcuts["horizontalalignment|hcenter"] = "Alt+Shift+H"
+shortcuts["horizontalalignment|right"] = "Alt+Shift+R"
+
+shortcuts["verticalalignment|bottom"] = "Alt+Shift+B"
+shortcuts["verticalalignment|baseline"] = "Alt+B"
+shortcuts["verticalalignment|vcenter"] = "Alt+Shift+C"
+shortcuts["verticalalignment|top"] = "Alt+Shift+T"
