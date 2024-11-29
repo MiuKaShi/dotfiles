2ae8cfa35e0875cdd99e84be06505eb77f1a3d2c
miuka <d.lolimiuku@gmail.com>
2024-11-29 16:38:05 +0900

    fix mamba & srt-deepl

--- .config/.config/mamba/mamba_env.sh
+++ .config/.config/mamba/mamba_env.sh
@@ -1,16 +1,13 @@
+#! /bin/zsh
 # >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-export MAMBA_EXE="/usr/bin/micromamba";
-export MAMBA_ROOT_PREFIX="/home/miuka/.mamba";
-__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+# !! Contents within this block are managed by 'micromamba shell init' !!
+export MAMBA_EXE='/usr/bin/micromamba'
+export MAMBA_ROOT_PREFIX='/home/miuka/.mamba'
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2>/dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
 else
-    if [ -f "/home/miuka/.mamba/etc/profile.d/micromamba.sh" ]; then
-        . "/home/miuka/.mamba/etc/profile.d/micromamba.sh"
-    else
-        export  PATH="/home/miuka/.mamba/bin:$PATH"  # extra space after export prevents interference from conda init
-    fi
+    alias micromamba="$MAMBA_EXE" # Fallback on help from micromamba activate
 fi
 unset __mamba_setup
 # <<< mamba initialize <<<
