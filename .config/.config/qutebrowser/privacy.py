eb12881bab47f599253c0f1b49152cadf939f8df
miuka <d.lolimiuku@gmail.com>
2025-07-03 00:31:56 +0900

    update qutebrowser

--- .config/.config/qutebrowser/privacy.py
+++ .config/.config/qutebrowser/privacy.py
@@ -11,7 +11,7 @@ c.content.webrtc_ip_handling_policy = "default-public-interface-only"
 c.content.site_specific_quirks.enabled = False
 c.content.headers.do_not_track = True
 c.content.headers.referer = "same-domain"
-c.content.headers.user_agent = "Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {qt_key}/{qt_version} {upstream_browser_key}/122.0.0.0 Safari/{webkit_version}"
+# c.content.headers.user_agent = "Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {qt_key}/{qt_version} {upstream_browser_key}/122.0.0.0 Safari/{webkit_version}"
 
 url_dict = {
     "js": [
